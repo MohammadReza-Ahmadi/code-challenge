d9e7632d550f4018e33e933c617d90741a8e447b
Mohammadreza Ahmadi <reza@Mohammadrezas-MacBook-Pro.local>
2022-06-28 22:07:10 +0200

    Add booking.com and some other changes

--- src/main/java/BookingDotComFlightCodeChallenge.java
+++ src/main/java/BookingDotComFlightCodeChallenge.java
@@ -5,17 +5,19 @@ public class BookingDotComFlightCodeChallenge {
 
     public static class Flight{
 
-        public Flight(long id, String name, String source, String dest) {
+        public Flight(Long id, String name, String source, String dest, Date flightDate) {
             this.id = id;
             this.name = name;
             this.source = source;
             this.dest = dest;
+            this.flightDate = flightDate;
         }
 
-        public long id;
+        public Long id;
         public String name;
         public String source;
         public String dest;
+        public Date flightDate;
 
         @Override
         public boolean equals(Object o) {
@@ -25,11 +27,12 @@ public class BookingDotComFlightCodeChallenge {
             if (o == null || getClass() != o.getClass())
                 return false;
 
-            Flight flight = (Flight) o;
-            return id == flight.id &&
-                    Objects.equals(name, flight.name) &&
-                    Objects.equals(source, flight.source) &&
-                    Objects.equals(dest, flight.dest);
+            Flight that = (Flight) o;
+            return  (id == null || that.id == null || Objects.equals(id, that.id)) &&
+              (name == null || that.name == null || name.equalsIgnoreCase(that.name)) &&
+              (source == null || that.source == null || source.equalsIgnoreCase(that.source)) &&
+              (dest == null || that.dest == null || dest.equalsIgnoreCase(that.dest)) &&
+              (flightDate == null || that.flightDate == null || Objects.equals(flightDate, that.flightDate));
         }
 
         @Override
@@ -72,7 +75,8 @@ public class BookingDotComFlightCodeChallenge {
             if (source == null || source.isEmpty() || map.get(source)==null){
                 return new ArrayList<>();
             }
-            return map.get(source);
+            Flight f = new Flight(null, null, source, null, null);
+            return mapFlight.get(f);
         }
 
         public List<Flight> searchBySourceAndDest(String source, String dest){
@@ -80,11 +84,29 @@ public class BookingDotComFlightCodeChallenge {
                 return new ArrayList<>();
             }
 
-            Flight f = new Flight(1, "F1", source, dest);
+            Flight f = new Flight(null, null, source, dest, null);
             return mapFlight.get(f);
         }
     }
 
+
+    public static void main(String[] args) {
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(201,2);
+        map.put(200,3);
+        map.get(201);
+/*        Integer key = 10;
+        System.out.println("key= "+key);
+        int h;
+        h = key.hashCode();
+        System.out.println("keyHashCode= "+h);
+        int a = h >>> 16;
+        System.out.println("h >>> 16= "+a);
+        int ret = h ^ (h >>> 16);
+        System.out.println("ret= "+ret);*/
+
+
+    }
 
 
 
--- src/test/java/BookingDotComFlightCodeChallengeTest.java
+++ src/test/java/BookingDotComFlightCodeChallengeTest.java
@@ -10,8 +10,8 @@ class BookingDotComFlightCodeChallengeTest {
     @Test
     public void test1(){
         List<BookingDotComFlightCodeChallenge.Flight> flights = List.of(
-                new BookingDotComFlightCodeChallenge.Flight(1,"F1","SA","DA"),
-                new BookingDotComFlightCodeChallenge.Flight(2,"F2","SB","DB")
+                new BookingDotComFlightCodeChallenge.Flight(1L,"F1","SA","DA", null),
+                new BookingDotComFlightCodeChallenge.Flight(2L,"F2","SB","DB", null)
         );
 
         BookingDotComFlightCodeChallenge.FlightSearch search = new BookingDotComFlightCodeChallenge.FlightSearch(flights);
@@ -21,8 +21,8 @@ class BookingDotComFlightCodeChallengeTest {
     @Test
     public void test2(){
         List<BookingDotComFlightCodeChallenge.Flight> flights = List.of(
-                new BookingDotComFlightCodeChallenge.Flight(1,"F1","SA","DA"),
-                new BookingDotComFlightCodeChallenge.Flight(2,"F2","SA","DB")
+                new BookingDotComFlightCodeChallenge.Flight(1L,"F1","SA","DA", null),
+                new BookingDotComFlightCodeChallenge.Flight(2L,"F2","SA","DB", null)
         );
 
         BookingDotComFlightCodeChallenge.FlightSearch search = new BookingDotComFlightCodeChallenge.FlightSearch(flights);
@@ -30,14 +30,57 @@ class BookingDotComFlightCodeChallengeTest {
     }
 
     @Test
-    public void testByObjectKey(){
+    public void testSearchByFlightKey1(){
         List<BookingDotComFlightCodeChallenge.Flight> flights = List.of(
-                new BookingDotComFlightCodeChallenge.Flight(1,"F1","S1","D1"),
-                new BookingDotComFlightCodeChallenge.Flight(1,"F2","S2","D2")
+                new BookingDotComFlightCodeChallenge.Flight(1L,"F1","S1","D1", null),
+                new BookingDotComFlightCodeChallenge.Flight(2L,"F2","S2","D2", null)
         );
 
         BookingDotComFlightCodeChallenge.FlightSearch search = new BookingDotComFlightCodeChallenge.FlightSearch(flights);
-        Assertions.assertEquals(1,search.searchBySourceAndDest("S1","D1").size());
+        List<BookingDotComFlightCodeChallenge.Flight> actual = search.searchBySourceAndDest("S1", "D1");
+        Assertions.assertEquals(1,actual.size());
+        Assertions.assertEquals(1,actual.get(0).id);
+        Assertions.assertEquals("S1",actual.get(0).source);
+        Assertions.assertEquals("D1",actual.get(0).dest);
+    }
+
+    @Test
+    public void testSearchByFlightKey2(){
+        List<BookingDotComFlightCodeChallenge.Flight> flights = List.of(
+                new BookingDotComFlightCodeChallenge.Flight(1L,"F1","S2","D1", null),
+                new BookingDotComFlightCodeChallenge.Flight(2L,"F2","S2","D2", null)
+        );
+
+        BookingDotComFlightCodeChallenge.FlightSearch search = new BookingDotComFlightCodeChallenge.FlightSearch(flights);
+        List<BookingDotComFlightCodeChallenge.Flight> actual = search.searchBySource( "S2");
+        Assertions.assertEquals(2,actual.size());
+    }
+
+    @Test
+    public void testSearchBySourceAndDest1(){
+        List<BookingDotComFlightCodeChallenge.Flight> flights = List.of(
+                new BookingDotComFlightCodeChallenge.Flight(1L,"F1","S1","D1", null),
+                new BookingDotComFlightCodeChallenge.Flight(1L,"F1","S1","D2", null)
+        );
+
+        BookingDotComFlightCodeChallenge.FlightSearch search = new BookingDotComFlightCodeChallenge.FlightSearch(flights);
+        List<BookingDotComFlightCodeChallenge.Flight> actual = search.searchBySourceAndDest( "S1","D1");
+        Assertions.assertEquals(1,actual.size());
+        Assertions.assertEquals(1,actual.get(0).id);
+        Assertions.assertEquals("S1",actual.get(0).source);
+        Assertions.assertEquals("D1",actual.get(0).dest);
+    }
+
+//    @Test
+    public void testSearchBySource1(){
+        List<BookingDotComFlightCodeChallenge.Flight> flights = List.of(
+                new BookingDotComFlightCodeChallenge.Flight(1L,"F1","S1","D1", null),
+                new BookingDotComFlightCodeChallenge.Flight(1L,"F1","S1","D2", null)
+        );
+
+        BookingDotComFlightCodeChallenge.FlightSearch search = new BookingDotComFlightCodeChallenge.FlightSearch(flights);
+        List<BookingDotComFlightCodeChallenge.Flight> actual = search.searchBySource( "S1");
+        Assertions.assertEquals(2,actual.size());
     }
 
 }
